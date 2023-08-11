b8aa0c9bdbe3893a8c3e48bd3f519a4a0eb17ee4
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-11 20:47:02 -0300

    update

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -14,12 +14,12 @@ public class MyFrame extends JFrame implements KeyListener {
         this.setLayout(null);
         this.addKeyListener(this);
 
-        icon = new ImageIcon("lancamento-do-foguete.png");
+        icon = new ImageIcon("lancamento-do-foguete(1).png");
         /*java.net.URL imgUrl = getClass().getResource("lancamento-do-foguete(1).png");
         ImageIcon icon = new ImageIcon(imgUrl);*/
 
         label = new JLabel();
-        label.setBounds(0,0,100,100);
+        label.setBounds(0,0,200,200);
         label.setIcon(icon);
         //label.setBackground(Color.red);
         //label.setOpaque(true);
